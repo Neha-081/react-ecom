c669e09466c28f48b1cf07645c533842cb8ed6b9
Neha <nehaprajapati1150@gmail.com>
2021-12-28 13:49:30 +0530

    shop page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SHOP_DATA from './shop.data'
+import CollectionPreview from '../../components/preview-collection/collection-preview.component';
 
 class ShopPage extends React.Component{
     constructor(props){
@@ -10,4 +11,18 @@ class ShopPage extends React.Component{
 
         }
     }
+
+    render(){
+        const {collections}=this.state
+        return (<div className='shop-page'>
+        {
+            collections
+            .map(({id,...otherCollectionProps})=>(
+                <CollectionPreview key={id} {...otherCollectionProps} />
+            ))
+        }
+        </div>)
+    }
 }
+
+export default ShopPage;
