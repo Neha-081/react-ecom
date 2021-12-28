00249f09e127e0fb59d2e00e71a0f3d78fd76a9e
Neha <nehaprajapati1150@gmail.com>
2021-12-28 14:07:42 +0530

    styling

--- src/components/preview-collection/collection-preview.component.jsx
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -1,4 +1,5 @@
 import './collection-preview.styles.scss';
+import CollectionItem from '../collection-item/collection-item.component';
 
 const CollectionPreview=({title,items})=>(
     <div className='collection-preview'>
@@ -7,8 +8,8 @@ const CollectionPreview=({title,items})=>(
         {
             items
             .filter((item,idx)=>idx<4)
-            .map(item=>(
-                <div key={item.id}>{item.name}</div>
+            .map(({id,...otherItemProps})=>(
+                <CollectionItem key={id} {...otherItemProps}/>
             ))
         }
         </div>
