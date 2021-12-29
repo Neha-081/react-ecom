5cb11020058bcfce9a858d6d1474897b9d23fc1c
Neha <nehaprajapati1150@gmail.com>
2021-12-30 00:25:12 +0530

    redux setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+//provider is component class which passes tore and gives context to whole application 
 import {Provider} from 'react-redux';
 
 
 import './index.css';
 import App from './App';
+import store from './redux/store'
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
   <BrowserRouter>
     <App />
   </BrowserRouter>
