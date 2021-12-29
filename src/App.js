b2bfabac2dfa201e1ed728623ef2ec51160bea84
Neha <nehaprajapati1150@gmail.com>
2021-12-29 16:50:12 +0530

    rendering user data of auth from firebase

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import {auth} from './firebase/firebase.utils';
+import {auth,createUserProfile} from './firebase/firebase.utils';
 
 class App extends React.Component{
   constructor(){
@@ -21,9 +21,22 @@ unsubscribeFromAuth=null;
 
 
   componentDidMount(){
-    this.unsubscribeFromAuth= auth.onAuthStateChanged(user=>{
-      this.setState({currentUser:user});
-      console.log(user);
+    this.unsubscribeFromAuth= auth.onAuthStateChanged(async userAuth=>{
+   if(userAuth){
+    const userRef=await createUserProfile(userAuth)  //check if our database has updated at that reference with any new data.
+    userRef.onSnapshot(snapShot=>{
+   this.setState({
+     currentUser:{
+       id:snapShot.id,
+       ...snapShot.data()
+     }
+   })
+
+    }) ;
+
+   
+    }
+    this.setState({currentUser:userAuth});   //currentuser to null
     })
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -10,7 +10,33 @@ const config={
     messagingSenderId: "952293346794",
     appId: "1:952293346794:web:d5b0e84588ab4f96703693",
     measurementId: "G-4VQHJR7WJE"
+  };
+
+
+export const createUserProfile=async(userAuth,additionalData)=>{
+  if(!userAuth) return;
+  const userRef=firestore.doc(`users/${userAuth.uid}`)
+
+  const snapShot=await userRef.get()
+  
+  if(!snapShot.exists){
+    const {displayName,email}=userAuth;
+    const createdAt=new Date();
+
+    try{
+    await userRef.set({
+      displayName,
+      email,
+      createdAt,
+      ...additionalData
+    })
+    }catch(error){
+      console.log('error',error);
+
+    }
   }
+  return userRef;
+}
 
   firebase.initializeApp(config);
 
