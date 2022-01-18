0f83e784392c939e78cbeefd948cf0e0fd73d27b
Neha <nehaprajapati1150@gmail.com>
2022-01-18 14:23:36 +0530

    nested routing in shop page

---
+++ src/pages/category/category.component.jsx
@@ -0,0 +1,12 @@
+// import './category.styles.scss';
+// import CollectionItem from '../../components/collection-item/collection-item.component'
+
+const CategoryPage=({match})=>{
+console.log(match);
+return(
+    <div className='category'>
+        <h2>CATEGORY PAGE</h2>
+    </div>
+)}
+
+export default CategoryPage;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom/cjs/react-router-dom.min';
 import CollectionOverview from '../../components/collections-overview/collections-overview.component'
+import CategoryPage from '../category/category.component';
 
-const ShopPage=({collections})=>(
+const ShopPage=({match})=>(
 <div className='shop-page'>
-<CollectionOverview/>
-        </div>
+<Route exact path={`${match.path}`} component={CollectionOverview}/>
+{/* it actually allows us to access this category ID as a parameter on the match object when we're inside of our category page. */}
+<Route path={`${match.path}/:categoryId`} component={CategoryPage}/>        
+</div>
     )
 
 
